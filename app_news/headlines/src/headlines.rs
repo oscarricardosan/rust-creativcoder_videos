1ce61d6b3e64099a52b810db194ffade202a920f
Oscar Sancheze <oscarricardosan@gmail.com>
2021-11-20 13:58:45 -0500

    Separo los archivos en módulos para tener un código más ordenado y retiro todos los warning

--- app_news/headlines/src/headlines.rs
+++ app_news/headlines/src/headlines.rs
@@ -1,22 +1,9 @@
-use std::borrow::Cow;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{Receiver, Sender, SyncSender};
-use eframe::egui;
-use eframe::egui::{Button, Color32, CtxRef, FontDefinitions, FontFamily, Hyperlink, Label, Layout, ScrollArea, Separator, TextStyle, Ui, Window};
-use eframe::egui::Align::Min;
-use eframe::egui::Key::Enter;
-use eframe::egui::TextStyle::Monospace;
-use crate::TopBottomPanel;
 use serde::{Serialize, Deserialize};
 
-pub const PADDING: f32= 3.0;
 
-const CONFY_FILE: &str= "headlines";
-
-const WHITE: Color32= Color32::from_rgb(255, 255, 255);
-const BLACK: Color32= Color32::from_rgb(0, 0, 0);
-const CYAN: Color32= Color32::from_rgb(0, 255, 255);
-const RED: Color32= Color32::from_rgb(255, 0, 0);
+pub const CONFY_FILE: &str= "headlines";
 
 pub enum Msg {
     ExecuteFetch,
@@ -71,41 +58,6 @@ impl Headlines {
         }
     }
 
-    pub fn configure_fonts(&self, ctx: &CtxRef) {
-        let mut font_def = FontDefinitions::default();
-
-        //Agregamos nuestra fuente personalizada
-        font_def.font_data.insert(
-            "MesloLGS".to_string(),
-            Cow::Borrowed(include_bytes!("../../MesloLGS_NF_Regular.ttf"))
-        );
-        //Fuente de Cabecera proporcional de 35 puntos
-        font_def.family_and_size.insert(
-            TextStyle::Heading,
-            (
-                FontFamily::Proportional,
-                28.
-            )
-        );
-        //Fuente de Body proporcional de 35 puntos
-        font_def.family_and_size.insert(
-            TextStyle::Body,
-            (
-                FontFamily::Proportional,
-                15.
-            )
-        );
-
-        //Datle prioridad a nuestra fuente en el Front
-        font_def.fonts_for_family
-            .get_mut(&FontFamily::Proportional)
-            .unwrap()
-            .insert(0, "MesloLGS".to_string());
-
-        ctx.set_fonts(font_def);
-
-    }
-
     pub fn fetch_news(&mut self) {
         if let Some(fetch_receiver) = &self.fetch_receiver {
             match fetch_receiver.try_recv(){
@@ -135,110 +87,6 @@ impl Headlines {
         }
     }
 
-    fn clear_news_cards(&mut self) {
-        self.articles= vec![];
-    }
-
-    pub fn render_news_cards(&self, _ctx:&CtxRef, ui: &mut Ui) {
-        //https://docs.rs/egui/0.15.0/egui/containers/struct.ScrollArea.html
-        ScrollArea::both()
-            .auto_shrink([false; 2])
-            .max_height(570.)
-            .show(ui, |ui| {
-                let total_elements= self.articles.len();
-                let mut count_executions= 0;
-                for news_card in &self.articles {
-                    ui.add_space(PADDING);
-                    //Render title
-                    let title= format!("► {}", news_card.title);
-                    if self.config.dark_mode {
-                        ui.colored_label(WHITE, title);
-                    }else{
-                        ui.colored_label(BLACK, title);
-                    }
-                    //Render desc
-                    ui.add_space(PADDING);
-                    let desc= Label::new(&news_card.desc)
-                        .text_style(TextStyle::Button);
-                    ui.add(desc);
-
-                    //Render hyperlynks
-                    if self.config.dark_mode {
-                        ui.style_mut().visuals.hyperlink_color = CYAN;
-                    } else {
-                        ui.style_mut().visuals.hyperlink_color = RED;
-                    }
-                    ui.add_space(PADDING);
-
-                    let mut layout = Layout::right_to_left();
-                    layout = layout.with_cross_align(Min);
-                    ui.with_layout(layout, |ui| {
-                        ui.add(Hyperlink::new(&news_card.url).text("Leer mas ⤴"));
-                    });
-                    ui.add_space(PADDING);
-                    count_executions+=1;
-                    if count_executions < total_elements{
-                        ui.add(Separator::default());
-                    }
-                }
-            });
-    }
-
-    pub fn render_top_panel(&mut self, ctx: &CtxRef, frame: &mut eframe::epi::Frame<'_>) {
-        TopBottomPanel::top("top_panel")
-            .show(ctx, |ui| {
-                ui.add_space(5.);
-                egui::menu::bar(ui, |ui|{
-                    //Logo
-                    ui.with_layout(
-                        Layout::left_to_right(), |ui|{
-                            ui.add(
-                                Label::new("📕").text_style(egui::TextStyle::Heading)
-                            );
-                        }
-                    );
-                    //Controls
-                    ui.with_layout(
-                        Layout::right_to_left(), |ui|{
-                            let close_btn= ui.add(
-                                Button::new("❌").text_style(TextStyle::Body)
-                            );
-                            if close_btn.clicked() {
-                                frame.quit();
-                            }
-
-                            let refresh_btn= ui.add(
-                                Button::new("🔄").text_style(TextStyle::Body)
-                            );
-                            if refresh_btn.clicked() {
-                                match &self.fetch_sender {
-                                    Some(fetch_sender)=> {
-                                        fetch_sender.lock().unwrap().send(Msg::ExecuteFetch).unwrap();
-                                    }
-                                    None=> {}
-                                }
-                            }
-
-                            let theme_btn= ui.add(
-                                Button::new({
-                                    if self.config.dark_mode {
-                                        "☀"
-                                    }else{
-                                        "🌜"
-                                    }
-                                }).text_style(TextStyle::Body)
-                            );
-                            if theme_btn.clicked() {
-                                self.config.dark_mode= !self.config.dark_mode;
-                            }
-                        }
-                    );
-
-                });
-            ui.add_space(5.);
-        });
-    }
-
     pub fn load_articles(&mut self) {
         if let Some(news_receiver) = &self.news_receiver {
             match news_receiver.try_recv(){
@@ -250,63 +98,5 @@ impl Headlines {
                 }
             }
         }
-    }
-
-    pub fn render_config(&mut self, ctx: &CtxRef) {
-        Window::new("Configuración").show(ctx, |ui|{
-            ui.label("Entra tu API_KEY para newsapi.org");
-
-            //El valor del input se ve reflejado en self.config.api_key
-            let text_input = ui.text_edit_singleline(&mut self.config.api_key);
-            if text_input.lost_focus() && ui.input().key_pressed(Enter){
-                if let Err(e) = confy::store(CONFY_FILE, HeadlinesConfig{
-                    dark_mode: self.config.dark_mode,
-                    api_key: self.config.api_key.to_string()
-                }) {
-                    tracing::error!("Fallo al guardar el estado de la app {}", e);
-                };
-                tracing::error!("Api key guardado");
-                self.api_key_initialized= true;
-                if let Some(fetch_sender) = &self.fetch_sender {
-                    fetch_sender.lock().unwrap().send(Msg::ExecuteFetch).unwrap();
-                }
-            }
-            tracing::error!("{}", &self.config.api_key);
-            ui.label("Si tu no te has registrado para obtener tu KEY, ve a");
-            ui.hyperlink("https://newsapi.org");
-
-        });
-    }
-
-    pub fn render_footer(&mut self, _ui: &mut Ui, ctx: &CtxRef) {
-        TopBottomPanel::bottom("bottom_panel")
-            .min_height(70.)
-            .max_height(70.)
-            .show(ctx, |ui|{
-                ui.vertical_centered(|ui|{
-                    ui.add_space(10.);
-                    ui.add(Label::new("API fuente: newsapi.org").monospace());
-                    ui.add(
-                        Hyperlink::new("https://github.com/emilk/egui")
-                            .text("Hecho con egui")
-                            .text_style(Monospace)
-                    );
-                    ui.add(
-                        Hyperlink::new("https://github.com/emilk/egui")
-                            .text("oscarricardosan/rust-creativcoder_videos")
-                            .text_style(Monospace)
-                    );
-                    ui.add_space(10.);
-                });
-            });
-    }
-
-    pub fn render_header(&mut self, ui: &mut Ui) {
-        ui.vertical_centered(|ui| {
-            ui.heading("Headlines");
-        });
-        ui.add_space(self::PADDING*5.1);
-        // let sep= Separator::default().spacing(10 as f32);
-        // ui.add(sep);
     }
 }
--- app_news/headlines/src/main.rs
+++ app_news/headlines/src/main.rs
@@ -1,76 +1,10 @@
 mod headlines;
+mod headlines_app;
+mod hadlines_render;
 
-use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{channel, sync_channel};
-use eframe::egui::{CentralPanel, CtxRef, TopBottomPanel, Vec2, Visuals};
-use eframe::epi::{App, Frame, Storage};
+use eframe::egui::{Vec2};
 use eframe::{NativeOptions, run_native};
 use crate::headlines::{Headlines, Msg};
-
-
-impl App for Headlines {
-
-    //Lllamado de una sola vez para configurar la app
-    fn setup(&mut self, ctx: &CtxRef, _frame: &mut Frame<'_>, _storage: Option<&dyn Storage>) {
-
-        let (news_sender, news_receiver)= channel();
-        let (fetch_sender, fetch_receiver)= sync_channel(1);
-
-        self.news_receiver = Some(news_receiver);
-        self.news_sender = Some(Arc::new(Mutex::new(news_sender)));
-
-        self.fetch_receiver = Some(fetch_receiver);
-        let fetch_sender = Arc::new(Mutex::new(fetch_sender));
-        self.fetch_sender = Some(fetch_sender.clone());
-
-        if self.api_key_initialized {
-            fetch_sender
-                .lock()
-                .unwrap()
-                .send(Msg::ExecuteFetch)
-                .unwrap();
-        }
-
-        self.configure_fonts(ctx);
-
-    }
-
-    //Este refresh se ejecuta 60 veces por segudo / 60fps
-    fn update(&mut self, ctx: &CtxRef, frame: &mut Frame<'_>) {
-
-        //Por defecto egui no escucha procesos si no tiene el focus
-        //para evitar consumo de recursos, pero esto hace que
-        //no se renderice sin el mouse encima, con la línea de abajo
-        //se soluciona el proceso.
-        ctx.request_repaint();
-
-        if self.config.dark_mode {
-            ctx.set_visuals(Visuals::dark());
-        }else {
-            ctx.set_visuals(Visuals::light());
-        }
-
-        if !self.api_key_initialized {
-            self.render_config(ctx);
-        }else {
-            self.load_articles();
-
-            CentralPanel::default()
-                .show(ctx, |ui| {
-                self.render_header(ui);
-                self.render_news_cards(ctx, ui);
-                self.render_footer(ui, ctx);
-            });
-        }
-        self.render_top_panel(ctx, frame);
-        self.fetch_news();
-    }
-
-    fn name(&self) -> &str {
-        "Headlines"
-    }
-
-}
 
 fn main() {
     tracing_subscriber::fmt::init();
